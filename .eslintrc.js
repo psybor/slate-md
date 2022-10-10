8f93d6a2aa2e86de8406b13100f01fb124a3bb92
章勇 <zhangyong@zhangyong.com>
2022-10-10 22:54:41 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+  },
+};
